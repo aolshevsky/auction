7ebd5596a70353fca3900b17a8f30baaa998e030
Alexey Olshevsky <campapp.games@gmail.com>
2019-11-03 17:09:08 +0300

    Create base controller moving

---
+++ AuctionApp/Controllers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AuctionApp
+//
+//  Created by Alexey Olshevsky on 11/3/19.
+//  Copyright © 2019 Alexey Olshevsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func moveButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "AuthViewController", bundle: nil)
+        let vc =  storyboard.instantiateInitialViewController() as? AuthViewController
+        if let vc = vc {
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true)
+        }
+    }
+    
+}
+
