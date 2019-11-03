bf7d64a8008b25ca5cfedd9e6ced78acb2d7af0f
Alexey Olshevsky <aolshevsky@comscore.com>
2019-11-03 22:41:51 +0300

    Update Login and Register pages

---
+++ AuctionApp/Controllers/Auth/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  AuctionApp
+//
+//  Created by Alexey Olshevsky on 11/3/19.
+//  Copyright © 2019 Alexey Olshevsky. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var useremailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        let userEmail = useremailTextField.text
+        let userPassword = userPasswordTextField.text
+        
+        if checkUserByUserEmailPassword(email: userEmail!, password: userPassword!) {
+            displaySuccessMessage(vc: self, message: "You are authorized")
+        }
+    }
+    
+    
+    func checkUserByUserEmailPassword(email: String, password: String) -> Bool{
+        return true;
+    }
+}
---
+++ AuctionApp/Controllers/Auth/RegisterViewController.swift
@@ -0,0 +1,46 @@
+//
+//  RegisterPageViewController.swift
+//  AuctionApp
+//
+//  Created by Alexey Olshevsky on 11/3/19.
+//  Copyright © 2019 Alexey Olshevsky. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var userEmailtextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        let userEmail = userEmailtextField.text
+        let userPassword = userPasswordTextField.text
+        let userRepeatPassword = repeatPasswordTextField.text
+        
+        // Check for empty field
+        if let userEmail = userEmail, let userPassword = userPassword, let userRepeatPassword = userRepeatPassword, userEmail.isEmpty || userPassword.isEmpty || userRepeatPassword.isEmpty {
+            displayAlertMessage(vc: self, message: "All fields are required")
+            return
+        }
+        
+        // Check if passwords match
+        if userRepeatPassword != userRepeatPassword {
+            displayAlertMessage(vc: self, message: "Passwords don't match")
+        }
+        
+        // Store data
+        
+        // Success
+        displaySuccessMessage(vc: self, message: "Registration is successful. Thank you!")
+    }
+}
