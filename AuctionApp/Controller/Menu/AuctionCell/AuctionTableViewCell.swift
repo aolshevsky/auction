453b9aafeb4971a2042fa1877521254aa4ea7542
Alexey Olshevsky <campapp.games@gmail.com>
2019-11-09 23:02:37 +0300

    Animation trigered star

--- AuctionApp/Controller/Menu/AuctionCell/AuctionTableViewCell.swift
+++ AuctionApp/Controller/Menu/AuctionCell/AuctionTableViewCell.swift
@@ -7,40 +7,58 @@
 //
 
 import UIKit
+import Lottie
 
 class AuctionTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var auctionImageView: UIImageView!
     @IBOutlet weak var priceLabel: UILabel!
-    @IBOutlet weak var starImageView: UIImageView!
+    @IBOutlet weak var animView: AnimationView!
     
     
     weak var cellAuction: Auction!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        starImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapStarImage)))
-        starImageView.isUserInteractionEnabled = true
-    }
-        
-    @objc func tapStarImage() {
-        cellAuction.isStar = !cellAuction.isStar
-        starImageView.image = getAuctionStarImage(isStar: cellAuction.isStar)
-        print("Imageview Clicked " + cellAuction.title)
+//        starImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapStarImage)))
+//        starImageView.isUserInteractionEnabled = true
     }
     
-    func getAuctionStarImage(isStar: Bool) -> UIImage {
-        if isStar {
-            return Images.star!
-        }
-        return Images.empty_star!
+    func setStarAnimation(state: Bool) {
+        addSwitchAnimation(filename: "star", state: state)
     }
+    
+    
     func setAuction(auction: Auction) {
         cellAuction = auction
         auctionImageView.image = auction.image
         titleLabel.text = auction.title
         priceLabel.text = String(auction.price) + " $"
-        starImageView.image = getAuctionStarImage(isStar: auction.isStar)
+        setStarAnimation(state: auction.isStar)
+    }
+    
+    
+    func addSwitchAnimation(filename: String, state: Bool) {
+        let animation = Animation.named(filename)
+        let animationSwitch = AnimatedSwitch()
+        animationSwitch.animation = animation
+        animationSwitch.animationSpeed = 1
+        animationSwitch.isOn = state
+        animationSwitch.setProgressForState(fromProgress: 0, toProgress: 1, forOnState: true)
+        animationSwitch.setProgressForState(fromProgress: 1, toProgress: 0, forOnState: false)
+        animationSwitch.addTarget(self, action: #selector(switchToggled), for: .valueChanged)
+        animationSwitch.contentMode = .scaleAspectFit
+        
+        animationSwitch.translatesAutoresizingMaskIntoConstraints = false
+        animView.addSubview(animationSwitch)
+        
+        NSLayoutConstraint.activate([
+            animationSwitch.heightAnchor.constraint(equalTo: animView.heightAnchor),
+            animationSwitch.widthAnchor.constraint(equalTo: animView.widthAnchor)])
+    }
+        
+    @objc func switchToggled(animatedSwitch: AnimatedSwitch) {
+        cellAuction.isStar = !cellAuction.isStar
     }
 }
